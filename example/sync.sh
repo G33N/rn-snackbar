ed24428531c865d98cb80f4c7691b2602b0dd3ff
Matias Celiz <mceliz@enlaceit.com.ar>
2020-08-01 22:40:30 -0300

    fix sync script

--- example/sync.sh
+++ example/sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Saved as its own file so that Travis uses the correct shell to run it.
-
+echo Create node_module/rn-snackbar
+mkdir node_modules
 mkdir node_modules/rn-snackbar
+echo Copy necesary files to example project
 cp -R ../{package.json,android,ios,lib} node_modules/rn-snackbar/
