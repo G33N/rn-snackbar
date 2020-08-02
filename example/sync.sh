9d613569c9748e12762c4ca99a09028935ca8676
Matias Celiz <mceliz@enlaceit.com.ar>
2020-08-02 02:04:15 -0300

    fix(package|sync)

--- example/sync.sh
+++ example/sync.sh
@@ -3,6 +3,7 @@
 # Saved as its own file so that Travis uses the correct shell to run it.
 echo Create node_module/rn-snackbar
 mkdir node_modules
-mkdir node_modules/rn-snackbar
+mkdir node_modules/@g33n
+mkdir node_modules/@g33n/rn-snackbar
 echo Copy necesary files to example project
-cp -R ../{package.json,android,ios,lib} node_modules/rn-snackbar/
+cp -R ../{package.json,android,ios,lib} node_modules/@g33n/rn-snackbar/
